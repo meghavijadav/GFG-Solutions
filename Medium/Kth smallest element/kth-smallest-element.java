e05d7ed2dcfbbc08cb5f2502462f01d2b7bdcdaf
Meghavi Jadav <100013496+meghavijadav@users.noreply.github.com>
2024-01-11 22:29:26 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Kth smallest element/kth-smallest-element.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    
+		    int arr[]=new int[n];
+		    
+		    for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+		    
+		    int k=sc.nextInt();
+		    Solution ob = new Solution();
+		    out.println(ob.kthSmallest(arr, 0, n-1, k));
+		}
+		out.flush();
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int K) 
+    { 
+       if (K > 0 && K <= r - l + 1) {
+            int pos = partition(arr, l, r);
+
+            if (pos - l == K - 1) {
+                return arr[pos];
+            }
+
+            if (pos - l > K - 1) {
+                return kthSmallest(arr, l, pos - 1, K);
+            }
+
+            return kthSmallest(arr, pos + 1, r, K - pos + l - 1);
+        }
+
+        return Integer.MAX_VALUE; // Return a value indicating that K is out of bounds
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
