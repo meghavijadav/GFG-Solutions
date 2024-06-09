cbc754912bb5117e42e701ec1daa4656d88c4d1c
Meghavi Jadav <100013496+meghavijadav@users.noreply.github.com>
2024-06-09 16:28:32 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Stock span problem/stock-span-problem.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+class Solution {
+    // Function to calculate the span of stockâ€™s price for all n days.
+    public static int[] calculateSpan(int price[], int n) {
+        // Your code here
+        int span[] = new int[n];
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1; //default
+        s.push(0);
+        
+        for(int i = 1; i < n; i++){
+            while(!s.isEmpty() && price[i] >= price[s.peek()]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                span[i] = i +1;
+            }else{
+                span[i] = i - s.peek();;
+            }
+            s.push(i);
+        }
+        return span;
+    }
+}
+
+//{ Driver Code Starts.
+
+class gfg {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+
+            int i = 0;
+            for (i = 0; i < n; i++) a[i] = sc.nextInt();
+
+            int[] s = new Solution().calculateSpan(a, n);
+
+            for (i = 0; i < n; i++) {
+                System.out.print(s[i] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
