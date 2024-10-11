af281a992ba24cccae0cc349b0f92e61e660611b
Meghavi Jadav <100013496+meghavijadav@users.noreply.github.com>
2024-10-11 22:46:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Last index of One/last-index-of-one.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String s = br.readLine();
+
+            Solution obj = new Solution();
+            System.out.println(obj.lastIndex(s));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int lastIndex(String s) {
+        int index = -1;
+        for(int i=0; i<s.length(); i++) index = s.charAt(i) == '1' ? i : index;
+        return index;
+    }
+}
